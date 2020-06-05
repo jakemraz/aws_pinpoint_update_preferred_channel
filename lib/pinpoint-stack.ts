f1a5cfeb8e536a9bc91c8a607ab42135fd5b4319
jakemraz <jakemraz100@gmail.com>
2020-06-05 17:24:39 +0900

    add pinpoint app

--- lib/pinpoint-stack.ts
+++ lib/pinpoint-stack.ts
@@ -1,10 +1,15 @@
 import * as cdk from '@aws-cdk/core';
-import * as ddb from '@aws-cdk/aws-dynamodb';
+import * as pinpoint from '@aws-cdk/aws-pinpoint';
 import * as constant from './interfaces/constant'
+
 
 export class PinpointStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const app = new pinpoint.CfnApp(this, `${constant.Namespace}PinpointApp`, {
+      name: `${constant.Namespace}PinpointApp`
+    });
 
   } 
 }

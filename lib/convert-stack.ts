f0ebc8c79ceadc72fa5535a1e627827820f36380
jakemraz <jakemraz100@gmail.com>
2020-06-18 02:06:32 +0900

    add ddb

--- lib/convert-stack.ts
+++ lib/convert-stack.ts
@@ -3,11 +3,22 @@ import * as ddb from '@aws-cdk/aws-dynamodb';
 import * as constant from './interfaces/constant'
 
 export class ConvertStack extends cdk.Stack {
+
+  public readonly preferredTable: ddb.Table;
+  public readonly userTable: ddb.Table;
+
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    
-    
+    this.preferredTable = new ddb.Table(this, `${constant.Namespace}PreferredTable`, {
+      tableName: `${constant.Namespace}PreferredTable`,
+      partitionKey: {name: 'client_id', type: ddb.AttributeType.STRING},
+      sortKey: {name: 'campaign_id', type: ddb.AttributeType.STRING}
+    });
 
+    this.userTable = new ddb.Table(this, `${constant.Namespace}UserTable`, {
+      tableName: `${constant.Namespace}UserTable`,
+      partitionKey: {name: 'user_id', type: ddb.AttributeType.STRING},
+    });
   }
 }

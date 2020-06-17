131db111fc05332cc5ea0358ed1495d67e004e02
jakemraz <jakemraz100@gmail.com>
2020-06-18 02:07:10 +0900

    add convert lambda

---
+++ lib/lambda-handler/preferred_update.py
@@ -0,0 +1,101 @@
+from __future__ import print_function
+
+import base64
+import boto3
+import json
+from botocore.exceptions import ClientError
+from boto3.dynamodb.table import BatchWriter
+
+print('Loading function')
+
+def lambda_handler(event, context):
+    output = []
+
+    ddb = boto3.resource('dynamodb')
+    table = ddb.Table('PreferredPreferredTable')
+    
+    items = []
+    duplicate = {}
+    
+
+    for record in event['records']:
+        print(record['recordId'])
+        payload = base64.b64decode(record['data'])
+        
+        print(payload)
+        
+        item = get_preferred_item(json.loads(base64.b64decode(record['data']).decode('utf-8')))
+        print(item)
+
+        if item is not None:
+            items.append(item)
+            key = item['client_id'] + item['campaign_id']
+            duplicate[key] = duplicate.get(key, 0) + 1
+
+        output_record = {
+            'recordId': record['recordId'],
+            'result': 'Ok',
+            'data': base64.b64encode(payload)
+        }
+        output.append(output_record)
+
+    with table.batch_writer() as batch:
+        for item in items:
+            
+            key = item['client_id'] + item['campaign_id']
+            if duplicate[key] == 0:
+                continue
+            elif duplicate[key] > 1 and item.get('read', None) == None:
+                # ignore 'send' if duplicated
+                continue
+            elif duplicate[key] > 1 and item.get('read', None) != None:
+                duplicate[key] = 0 # Don't use anymore
+
+            try:
+                batch.put_item(Item=item)
+
+            except ClientError as e:
+                print(e.response)
+
+
+    print('Successfully processed {} records.'.format(len(event['records'])))
+
+    return {'records': output}
+
+
+def get_preferred_item(payload):
+    item = {}
+    send_event = ['_campaign.send']
+    read_event = ['_email.open','_campaign.opened_notification']
+    
+    event_type = payload.get('event_type', None)
+    if event_type == None:
+        return
+    
+    if event_type not in send_event and event_type not in read_event:
+        return
+    
+    attributes = payload.get('attributes', None)
+    if attributes == None:
+        return
+    
+    item['client_id'] = payload['client']['client_id']
+    item['campaign_id'] = attributes['campaign_id']
+    
+    if event_type in send_event:
+        item['send'] = True
+        endpoint = json.loads(payload['client_context']['custom']['endpoint'])
+        item['channel'] = endpoint['ChannelType']
+        user_id = endpoint.get('User',{'UserId':''}).get('UserId','')
+        #item['user_id'] = user_id
+    else:
+        item['send'] = True
+        item['read'] = True
+        if event_type == '_email.open':
+            item['channel'] = 'EMAIL'
+        elif event_type == '_campaign.opened_notification':
+            item['channel'] = payload['endpoint']['ChannelType']
+        
+    
+    return item
+
--- lib/pinpoint_eventstream-stack.ts
+++ lib/pinpoint_eventstream-stack.ts
@@ -1,10 +1,27 @@
 import * as cdk from '@aws-cdk/core';
 import * as ddb from '@aws-cdk/aws-dynamodb';
+import * as iam from '@aws-cdk/aws-iam';
+import * as lambda from '@aws-cdk/aws-lambda';
 import * as constant from './interfaces/constant'
+import * as path from 'path';
 
 export class PinpointEventstreamStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+
+    const ddbPolicy = new iam.PolicyStatement({
+      actions: ["dynamodb:*"],
+      resources: ['*']
+    });
+
+
+    const preferredUpdateHandler = new lambda.Function(this, `${constant.Namespace}PreferredUpdateHandler`, {
+      functionName: `${constant.Namespace}PreferredUpdateHandler`,
+      runtime: lambda.Runtime.PYTHON_3_8,
+      handler: 'preferred_update.lambda_handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, 'lambda-handler'))
+    });
+    preferredUpdateHandler.addToRolePolicy(ddbPolicy);
   }
 }

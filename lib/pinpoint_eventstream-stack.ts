003513bfd4fa7c2d6d79afcbcb9e4ad23e8ffd9d
jakemraz <jakemraz100@gmail.com>
2020-06-22 17:28:03 +0900

    tmp

--- lib/pinpoint_eventstream-stack.ts
+++ lib/pinpoint_eventstream-stack.ts
@@ -11,36 +11,68 @@
   *  and limitations under the License.                                                                                *
   ******************************************************************************************************************** */ 
 
-import * as cdk from '@aws-cdk/core';
-import * as pinpoint from '@aws-cdk/aws-pinpoint';
-import * as ddb from '@aws-cdk/aws-dynamodb';
-import * as iam from '@aws-cdk/aws-iam';
-import * as lambda from '@aws-cdk/aws-lambda';
-import * as constant from './interfaces/constant'
-import * as path from 'path';
-
-
-interface Props extends cdk.StackProps{
-  pinpointApp: pinpoint.CfnApp;
-  preferredTable: ddb.ITable;
-}
-
-export class PinpointEventstreamStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props: Props) {
-    super(scope, id, props);
-
-    const ddbPolicy = new iam.PolicyStatement({
-      actions: ["dynamodb:*"],
-      resources: ['*']
-    });
-
-
-    const preferredUpdateHandler = new lambda.Function(this, `${constant.Namespace}EventstreamLambda`, {
-      functionName: `${constant.Namespace}EventstreamLambda`,
-      runtime: lambda.Runtime.PYTHON_3_8,
-      handler: 'eventstream_lambda.lambda_handler',
-      code: lambda.Code.fromAsset(path.join(__dirname, 'lambda-handler'))
-    });
-    preferredUpdateHandler.addToRolePolicy(ddbPolicy);
-  }
-}
+ import * as cdk from '@aws-cdk/core';
+ import * as pinpoint from '@aws-cdk/aws-pinpoint';
+ import * as ddb from '@aws-cdk/aws-dynamodb';
+ import * as iam from '@aws-cdk/aws-iam';
+ import * as lambda from '@aws-cdk/aws-lambda';
+ import * as constant from './interfaces/constant'
+ import * as firehose from '@aws-cdk/aws-kinesisfirehose'
+ import * as s3 from '@aws-cdk/aws-s3'
+ import * as path from 'path';
+ 
+ 
+ interface Props extends cdk.StackProps{
+   pinpointApp: pinpoint.CfnApp;
+   preferredTable: ddb.ITable;
+ }
+ 
+ export class PinpointEventstreamStack extends cdk.Stack {
+   constructor(scope: cdk.Construct, id: string, props: Props) {
+     super(scope, id, props);
+ 
+     new s3.Bucket(this, `${constant.Namespace}PinpointEventstreamS3`);
+ 
+     // const firehoseRole = new iam.Role(this, `${constant.Namespace}FirehoseDeliveryRole`, {
+     //   assumedBy: new iam.ServicePrincipal("firehose.amazonaws.com"),
+     // })
+     // firehoseRole.addToPolicy(new iam.PolicyStatement({
+     //   actions: [
+     //     "s3:AbortMultipartUpload",
+     //     "s3:GetBucketLocation",
+     //     "s3:GetObject",
+     //     "s3:ListBucket",
+     //     "s3:ListBucketMultipartUploads",
+     //     "s3:PutObject"
+     //   ]
+     // }));
+     
+ 
+     
+ 
+     // const fs = new firehose.CfnDeliveryStream(this, `${constant.Namespace}Firehose`, {
+     //   s3DestinationConfiguration: {
+     //     bucketArn: pinpointEventstreamS3.bucketArn,
+         
+     //   }
+ 
+     // });
+ 
+ 
+ 
+ 
+     const ddbPolicy = new iam.PolicyStatement({
+       actions: ["dynamodb:*"],
+       resources: ['*']
+     });
+ 
+     const preferredUpdateHandler = new lambda.Function(this, `${constant.Namespace}EventstreamLambda`, {
+       functionName: `${constant.Namespace}EventstreamLambda`,
+       runtime: lambda.Runtime.PYTHON_3_8,
+       handler: 'eventstream_lambda.lambda_handler',
+       code: lambda.Code.fromAsset(path.join(__dirname, 'lambda-handler'))
+     });
+     preferredUpdateHandler.addToRolePolicy(ddbPolicy);
+   }
+ }
+ 
